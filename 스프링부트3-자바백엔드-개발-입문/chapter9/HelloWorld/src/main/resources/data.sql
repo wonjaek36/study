f7df39a201faf61e4ebfc8422a675a72519780d8
Kim Wonjae <wonjaek36@Kimui-MacBookPro.local>
2023-08-20 17:00:43 +0900

    [스프링부트3 - chapter9] Remove id when use insert to member table

--- 스프링부트3-자바백엔드-개발-입문/chapter9/HelloWorld/src/main/resources/data.sql
+++ 스프링부트3-자바백엔드-개발-입문/chapter9/HelloWorld/src/main/resources/data.sql
@@ -2,6 +2,6 @@ INSERT INTO article(title, content) VALUES ('title1', 'content1');
 INSERT INTO article(title, content) VALUES ('title2', 'content2');
 INSERT INTO article(title, content) VALUES ('title3', 'content3');
 
-INSERT INTO member(id, username, password) VALUES (1, 'cat', 'dog');
-INSERT INTO member(id, username, password) VALUES (2, 'duck', 'goose');
-INSERT INTO membeR(id, username, password) VALUES (3, 'tiger', 'lion');
+INSERT INTO member(username, password) VALUES ('cat', 'dog');
+INSERT INTO member(username, password) VALUES ('duck', 'goose');
+INSERT INTO membeR(username, password) VALUES ('tiger', 'lion');
