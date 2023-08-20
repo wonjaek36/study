c9423a59146666b75950a5f0834ff9b4ce82cd58
Kim Wonjae <wonjaek36@Kimui-MacBookPro.local>
2023-08-20 16:56:44 +0900

    [스프링부트3 - chapter9] Following the book

---
+++ 스프링부트3-자바백엔드-개발-입문/chapter9/HelloWorld/src/main/java/com/example/firstproject/entity/Article.java
@@ -0,0 +1,26 @@
+package com.example.firstproject.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+@Entity
+public class Article {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    @Getter
+    private String title;
+
+    @Column
+    private String content;
+}
---
+++ 스프링부트3-자바백엔드-개발-입문/chapter9/HelloWorld/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO article(title, content) VALUES ('title1', 'content1');
+INSERT INTO article(title, content) VALUES ('title2', 'content2');
+INSERT INTO article(title, content) VALUES ('title3', 'content3');
+
+INSERT INTO member(id, username, password) VALUES (1, 'cat', 'dog');
+INSERT INTO member(id, username, password) VALUES (2, 'duck', 'goose');
+INSERT INTO membeR(id, username, password) VALUES (3, 'tiger', 'lion');
