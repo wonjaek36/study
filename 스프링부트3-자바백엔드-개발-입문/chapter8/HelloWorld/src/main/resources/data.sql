9b6e612670dfe4a7889cbd727cb1c194cf427e7d
Kim Wonjae <wonjaek36@Kimui-MacBookPro.local>
2023-08-20 16:31:01 +0900

    [스프링부트3] chapter8 / Add default 3 members

--- 스프링부트3-자바백엔드-개발-입문/chapter8/HelloWorld/src/main/resources/data.sql
+++ 스프링부트3-자바백엔드-개발-입문/chapter8/HelloWorld/src/main/resources/data.sql
@@ -1,3 +1,7 @@
 INSERT INTO article(id, title, content) VALUES (1, 'title1', 'content1');
 INSERT INTO article(id, title, content) VALUES (2, 'title2', 'content2');
 INSERT INTO article(id, title, content) VALUES (3, 'title3', 'content3');
+
+INSERT INTO member(id, username, password) VALUES (1, 'cat', 'dog');
+INSERT INTO member(id, username, password) VALUES (2, 'duck', 'goose');
+INSERT INTO membeR(id, username, password) VALUES (3, 'tiger', 'lion');
