9e3478b0f4a597887a2830b5efd704d3458ccc13
Kim Wonjae <wonjaek36@Kimui-MacBookPro.local>
2023-08-20 23:43:42 +0900

    [스프링부트3 - chapter11] Following the book

---
+++ 스프링부트3-자바백엔드-개발-입문/chapter11/HelloWorld/src/main/resources/data.sql
@@ -0,0 +1,7 @@
+INSERT INTO article(title, content) VALUES ('title1', 'content1');
+INSERT INTO article(title, content) VALUES ('title2', 'content2');
+INSERT INTO article(title, content) VALUES ('title3', 'content3');
+
+INSERT INTO member(username, password) VALUES ('cat', 'dog');
+INSERT INTO member(username, password) VALUES ('duck', 'goose');
+INSERT INTO membeR(username, password) VALUES ('tiger', 'lion');
