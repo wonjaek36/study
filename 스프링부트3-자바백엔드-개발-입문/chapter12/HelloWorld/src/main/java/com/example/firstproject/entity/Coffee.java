d58238b7b6da45583a2d8064090c7003b7f9342a
Kim Wonjae <wonjaek36@Kimui-MacBookPro.local>
2023-08-21 16:41:25 +0900

    [스프링부트3 - chapter12] SelfCheck / Add patch function to CoffeeEntity

--- 스프링부트3-자바백엔드-개발-입문/chapter12/HelloWorld/src/main/java/com/example/firstproject/entity/Coffee.java
+++ 스프링부트3-자바백엔드-개발-입문/chapter12/HelloWorld/src/main/java/com/example/firstproject/entity/Coffee.java
@@ -24,4 +24,11 @@ public class Coffee {
     @Column
     @Getter
     private Integer price;
+
+    public void patch(Coffee coffee) {
+        if (coffee.name != null)
+            this.name = coffee.name;
+        if (coffee.price != null)
+            this.price = coffee.price;
+    }
 }
