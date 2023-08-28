4aa848e202b416971b728bca805716d9c1807599
Kim Wonjae <wonjaek36@Kimui-MacBookPro.local>
2023-08-28 14:49:40 +0900

    [스프링부트3 - chapter15] Following the book

---
+++ 스프링부트3-자바백엔드-개발-입문/chapter15/HelloWorld/src/main/java/com/example/firstproject/dto/CoffeeDto.java
@@ -0,0 +1,18 @@
+package com.example.firstproject.dto;
+
+import com.example.firstproject.entity.Coffee;
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class CoffeeDto {
+
+    private Long id;
+    private String name;
+    private Integer price;
+
+    public Coffee toEntity() {
+        return new Coffee(id, name, price);
+    }
+}
