f5931a4dd10d0a28e4fb71142b83b90d3db1a9d9
Wonjae Kim <wonjaek36@gmail.com>
2023-10-20 00:17:08 +0900

    [AtomicKotlin] AppendixA Double eq func / neq func

--- Atomic_Kotlin/src/main/kotlin/appendix/atomictest/AtomicTest.kt
+++ Atomic_Kotlin/src/main/kotlin/appendix/atomictest/AtomicTest.kt
@@ -40,3 +40,21 @@ infix fun <T> T.eq(rval: T) {
     }
 }
 
+/**
+ * 어떤 Double 값이 rval에 지정된 Double 값과 같은지 비교한다.
+ * 두 값의 차이가 작은 양숫값 (0.0000001) 보다 작으면 두 Double을 같다고 판정한다.
+ */
+infix fun Double.eq(rval: Double) {
+    test(this, rval) {
+        abs(this - rval) < 0.0000001
+    }
+}
+
+/**
+ * this != 'rval'인지 검증한다
+ */
+infix fun <T> T.neq(rval: T) {
+    test(this, rval, checkEquals = false) {
+        this != rval
+    }
+}
