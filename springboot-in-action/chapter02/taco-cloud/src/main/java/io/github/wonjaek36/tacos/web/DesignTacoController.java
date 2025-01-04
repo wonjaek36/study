9bc47bc63803a632985cce32a6506858855a8c57
Wonjae Kim <wonjaek36@gmail.com>
2025-01-04 18:34:21 +0900

    [Chatper02] feat: Validate data class

--- springboot-in-action/chapter02/taco-cloud/src/main/java/io/github/wonjaek36/tacos/web/DesignTacoController.java
+++ springboot-in-action/chapter02/taco-cloud/src/main/java/io/github/wonjaek36/tacos/web/DesignTacoController.java
@@ -2,9 +2,11 @@ package io.github.wonjaek36.tacos.web;
 
 import io.github.wonjaek36.tacos.Ingredient;
 import io.github.wonjaek36.tacos.Taco;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,7 +24,10 @@ import io.github.wonjaek36.tacos.Ingredient.Type;
 public class DesignTacoController {
 
     @PostMapping
-    public String processDesign(Taco design) {
+    public String processDesign(@Valid Taco design, Errors errors) {
+        if (errors.hasErrors()) {
+            return "design";
+        }
         // 3장에서 타코 디자인 (선택된 식자재 내역)을 저장
         log.info("Processing design: " + design);
 
--- springboot-in-action/chapter02/taco-cloud/src/main/java/io/github/wonjaek36/tacos/web/OrderController.java
+++ springboot-in-action/chapter02/taco-cloud/src/main/java/io/github/wonjaek36/tacos/web/OrderController.java
@@ -1,9 +1,11 @@
 package io.github.wonjaek36.tacos.web;
 
 import io.github.wonjaek36.tacos.Order;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +22,11 @@ public class OrderController {
     }
 
     @PostMapping
-    public String processOrder(Order order) {
+    public String processOrder(@Valid Order order, Errors errors) {
+        if (errors.hasErrors()) {
+            return "orderForm";
+        }
+
         log.info("Order submitted: " + order);
         return "redirect:/";
     }
