6fbb730404403290ead14356b5605a057980e934
Wonjae Kim <wonjaek36@gmail.com>
2025-02-26 23:15:06 +0900

    feat: Chapter 15. subprocess

--- introducing_python/Chapter15/subprocess1.py
+++ introducing_python/Chapter15/subprocess1.py
@@ -8,3 +8,22 @@ print(f"date -u: {result}")
 
 result = subprocess.getoutput('date -u | wc')
 print(f"date -u | wc: {result}")
+
+result = subprocess.check_output(['date', '-u'])
+print(f"\ncheck_output / date -u: {result}")
+
+result = subprocess.getstatusoutput('date')
+print("\ngetstatusoutput: (exit code, result)")
+print(f"getstatusoutput / date: {result}")
+
+result = subprocess.call('date')
+print("\ncall: (only return) exit_code")
+print(f"call / date: {result}")
+
+result = subprocess.call('date -u', shell=True)
+print("\ncall / date: {result}")
+print(f"call / date: {result}")
+
+result = subprocess.call(['date', '-u'])
+print("\ncall with array / call(['date', '-u'])")
+print(f"call with array / date: {result}")
