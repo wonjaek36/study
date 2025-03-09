52f37402784336e087466a2c2d5510076e5e32a1
Wonjae Kim <wonjaek36@gmail.com>
2025-03-09 09:29:37 +0900

    feat(python): [Chapter15] run os funcs

---
+++ os1.py
@@ -0,0 +1,13 @@
+import os
+
+print ('os.uname()')
+print (os.uname(), '\n')
+
+print ('os.getloadavg()')
+print (os.getloadavg(), '\n')
+
+print ('os.cpu_count()')
+print (os.cpu_count(), '\n')
+
+print ('os.system(\'date -u\')')
+print (os.system('date -u'), '\n')
